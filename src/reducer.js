46a261204d3b3d7539cb7c119650eaa309e2b153
Kashaf Shehzadi <kashafshehzadi480@gmail.com>
2024-10-15 17:14:48 +0500

    Subtotal for addToCart

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,24 @@ export const initialState = {
     basket: []
 }
 
+//selector an advance way to doo so
+// export const getBasketTotal = (basket) => {
+//     return basket?.reduce((amount, item) => item.price + amount, 0);
+// }
+export const getBasketTotal = (basket) => {
+    let total = 0;
+
+    // Check if basket is not empty
+    if (basket && basket.length > 0) {
+        // Loop through each item in the basket
+        for (let i = 0; i < basket.length; i++) {
+            total += basket[i].price; // Add each item's price to the total
+        }
+    }
+
+    return total; // Return the total amount
+}
+
 const reducer = (state, action) => {
     console.log(action);
     switch (action.type) {
